134eb641d409736a14985e9cb597784f12aaad15
ajayrana0503 <ajayrana0503@gmail.com>
2023-01-10 23:30:56 +0530

    logger

--- xclusive_backend/xclusive/src/main/java/com/xclusive/controller/Demo.java
+++ xclusive_backend/xclusive/src/main/java/com/xclusive/controller/Demo.java
@@ -3,6 +3,8 @@ package com.xclusive.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
 public class Demo {
-
+	
+	Logger logger = LoggerFactory.getLogger(getClass());
+    
 	@GetMapping("/helloworld")
 	public Map<String, String> helloWorld() {
+		logger.error("Fatal error");
+		
 		System.out.println("hello world");
 		Map<String,String> mp=new HashMap<String,String>();
 		mp.put("101", "Ajay");
