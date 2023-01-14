0052830179e180e99b70d61c0bf7038fdee870f7
iamnobodyji@gmail.com <iamnobodyji@gmail.com>
2023-01-14 18:17:31 +0530

    css enhanced

--- xclusive_frontend/src/app/declaration.ts
+++ xclusive_frontend/src/app/declaration.ts
@@ -1,7 +1,7 @@
 
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component"
-import { DashboardComponent, FooterComponent, HeaderComponent, InputComponent, LoginButtonComponent, LoginComponent, PageErroComponent, SignUpComponent, SignUpReactiveComponent } from "./ui"
+import { DashboardComponent, InputComponent, LoginButtonComponent, LoginComponent, PageErroComponent, SignUpComponent, SignUpReactiveComponent } from "./ui"
 import { AppRoutingModule } from "./app-routing.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
@@ -11,8 +11,6 @@ import { LoginSignUpApiService } from "./services";
 export const component = [
   AppComponent,
   DashboardComponent,
-  FooterComponent,
-  HeaderComponent,
   LoginComponent,
   PageErroComponent,
   SignUpComponent,
